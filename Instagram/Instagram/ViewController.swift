449186c4c22214a3826fb283340c926211c3a81e
IamTort <61164603+IamTort@users.noreply.github.com>
2022-10-17 11:45:31 +0300

    init

---
+++ Instagram/Instagram/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by angelina on 17.10.2022.
+//
+
+import UIKit
+
+///  ViewController
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

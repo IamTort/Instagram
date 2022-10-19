fda4210b806e4b01a416d507db899ebf0339c8cf
IamTort <61164603+IamTort@users.noreply.github.com>
2022-10-19 13:48:13 +0300

    исправила

---
+++ Instagram/Instagram/Post.swift
@@ -0,0 +1,16 @@
+//
+//  Post.swift
+//  Instagram
+//
+//  Created by angelina on 19.10.2022.
+//
+
+import Foundation
+
+/// модель информации для запослнения поста
+struct Post {
+    let nickName: String
+    let image: String
+    let likeCount: String
+    let postText: String
+}
--- Instagram/Instagram/PostTableViewCell.swift
+++ Instagram/Instagram/PostTableViewCell.swift
@@ -10,11 +10,35 @@ import UIKit
 ///  Класс ячейки с постом
 final class PostTableViewCell: UITableViewCell {
 
-    // MARK: - Outlet
-    @IBOutlet weak var avatarImageView: UIImageView!
-    @IBOutlet weak var nickNameLabel: UILabel!
-    @IBOutlet weak var postImageView: UIImageView!
-    @IBOutlet weak var likeCountLabel: UILabel!
-    @IBOutlet weak var postLabel: UILabel!
-    @IBOutlet weak var commentImageView: UIImageView!
+    // MARK: - Private Outlet
+    @IBOutlet private weak var avatarImageView: UIImageView!
+    @IBOutlet private weak var nickNameLabel: UILabel!
+    @IBOutlet private weak var postImageView: UIImageView!
+    @IBOutlet private weak var likeCountLabel: UILabel!
+    @IBOutlet private weak var postLabel: UILabel!
+    @IBOutlet private weak var commentImageView: UIImageView!
+    
+    func setupData(post: Post) {
+        avatarImageView.image = UIImage(named: post.image)
+        nickNameLabel.text = post.nickName
+        postImageView.image = UIImage(named: post.image)
+        likeCountLabel.text = post.likeCount
+        postLabel.attributedText = setupDeliveryDateLabel(post: post)
+        commentImageView.image = UIImage(named: post.image)
+    }
+    
+    private func setupDeliveryDateLabel(post: Post) -> NSMutableAttributedString {
+        let myMutableString = NSMutableAttributedString(string: "\(post.nickName) \(post.postText)")
+        myMutableString.addAttribute(
+            NSAttributedString.Key.font,
+            value: UIFont.systemFont(ofSize: 12, weight: .bold),
+            range: NSRange(location: 1, length: post.nickName.count)
+        )
+        myMutableString.addAttribute(
+            NSAttributedString.Key.font,
+            value: UIFont.systemFont(ofSize: 12, weight: .regular),
+            range: NSRange(location: post.nickName.count + 1, length: post.postText.count)
+        )
+        return myMutableString
+    }
 }
